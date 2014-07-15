7d6301ba5dc632ee471a25e8370b1fc03e7baad4
Andrew Hills <ahills60@gmail.com>
2014-07-15 16:05:57 +0100

    Fixes lighting header file
    - Lighting header file has been corrected to use the material database.
    - Vector multiplication has been corrected.
    - Hit float arrays are now used.

--- lighting.h
+++ lighting.h
@@ -18,6 +18,8 @@ void specular(float lightDirection[3], float textureColour[3]);
 extern float HitData[18];
 extern float RGBChannels[3];
 extern float ResultStore[4][4];
+extern float Light[8];
+extern float MaterialDB[MAX_OBJECTS][19];
 
 // void checkColour(Vector colour, int stage)
 // {
@@ -36,51 +38,64 @@ extern float ResultStore[4][4];
 // }
 
 /* Creates ambiance effect given a hit, a scene and some light */
-void ambiance(Hit hit, Scene scene, Light light, float textureColour[3])
+void ambiance(float localHitData[18], float textureColour[3])
 {
-    float outputColour[3];
+    int i;
     
     // Check to see if there's a texture
     if (textureColour[0] < 0)
-        //TODO: Resolve the material object
-        //TODO: Unravel the objects within the scenery
-        outputColour = scene.object[hit.objectIndex].material.compAmbianceColour; // No texture. Apply material colour
+         // No texture. Apply material colour
         for (i = 0; i < 3; i += 1)
-            RGBChannels[i] += outputColour[i];
+            RGBChannels[i] += MaterialDB[localHitData[HitDataObjectIndex]][MaterialCompAmbianceColour + i];
     else
     {
-        scalarVecMult(scene.object[hit.objectIndex].material.ambiance, textureColour); // Texture. Apply texture colour
+        scalarVecMult(MaterialDB[localHitData[HitDataObjectIndex]][MaterialAmbiance], textureColour); // Texture. Apply texture colour
         for (i = 0; i < 3; i += 1)
             RGBChannels[i] += ResultStore[i];
     }
 }
 
 /* Creates diffusion effect given a hit, a scene and some light */
-void diffusion(Hit hit, Scene scene, Light light, float lightDirection[3], float textureColour[3])
+void diffusion(float localHitData[18], float lightDirection[3], float textureColour[3])
 {
+    float vector[3];
     int i;
     
-    if (scene.object[hit.objectIndex].material.diffusivity > 0)
+    if (MaterialDB[localHitData[HitDataObjectIndex]][MaterialDiffusive] > 0)
     {
+        for (i = 0; i < 3; i += 1)
+            vector[i] = localHitData[HitDataHitNormal + i];
+        
         // Need to compute the direction of light
-        float dotProduct = dot(hit.normal, lightDirection);
+        float dotProduct = dot(vector, lightDirection);
         
         // If the dot product is negative, this term shouldn't be included.
         if (dotProduct < 0)
             return;
         
         // Dot product is positive, so continue
-        float distance = dotProduct * scene.object[hit.objectIndex].material.diffusivity;
+        float distance = dotProduct * MaterialDB[localHitData[HitDataObjectIndex]][MaterialDiffusive];
         
         // Has a texture been defined?
         if (textureColour[0] < 0)
         {    
+            for (i = 0; i < 3; i += 1)
+                vector[i] = MaterialDB[localHitData[HitDataObjectIndex]][MaterialLightColour + i]
              // No texture defined
-            scalarVecMult(distance, scene.object[hit.objectIndex].material.matLightColour);
+            scalarVecMult(distance, vector);
         }
         else
         {
-            scalarVecMult(distance, textureColour * light.colour));
+            // Extract the light colour:
+            for (i = 0; i < 3; i += 1)
+                vector[i] = Light[LightColour + i];
+            // Combination of the texture colour and the material
+            vecMult(textureColour, vector);
+            // Extract the result from the result store
+            for (i = 0; i < 3; i += 1)
+                vector[i] = ResultStore[i];
+            
+            scalarVecMult(distance, vector);
         }
     }
     else 
@@ -92,32 +107,43 @@ void diffusion(Hit hit, Scene scene, Light light, float lightDirection[3], float
 }
 
 /* Creates specular effect given a hit, a scene and some light */
-void specular(Hit hit, Scene scene, Light light, float lightDirection[3], float textureColour[3])
+void specular(float localHitData[18], float lightDirection[3], float textureColour[3])
 {
     int i;
+    float vector[3], ray[6], dotproduct;
     
-    if (scene.object[hit.objectIndex].material.specular > 0)
+    if (MaterialDB[localHitData[HitDataObjectIndex]][MaterialSpecular] > 0)
     {
-        float dotProduct;
-    
         // Reflective ray:
-        Ray reflection = reflectRay(hit);
-        dotProduct = dot(lightDirection, reflection.direction);
+        reflectRay(localHitData);
+        for (i = 0; i < 6; i += 1)
+            ray[i] = ResultStore[i];
+        for (i = 0; i < 3; i += 1)
+            vector[i] = ray[RayDirectionx + i];
+        
+        dotProduct = dot(lightDirection, vector);
     
         if (dotProduct < 0)
             return;
     
-        float distance = fp_pow(dotProduct, scene.object[hit.objectIndex].material.shininess) * scene.object[hit.objectIndex].material.specular);
+        float distance = fp_pow(dotProduct, MaterialDB[localHitData[HitDataObjectIndex]][MaterialShininess]) * MaterialDB[localHitData[HitDataObjectIndex]][MaterialSpecular]);
             
         // Has a texture been defined?
         if (textureColour[0] < 0)
         {
-            scalarVecMult(distance, scene.object[hit.objectIndex].material.matLightColour); // No texture defined
+            for (i = 0; i < 3; i += 1)
+                vector[i] = MaterialDB[localHitData[HitDataObjectIndex]][MaterialLightColour + i] 
+            scalarVecMult(distance, vector); // No texture defined
         }
         else
         {
-            vecMult(textureColour, light.colour)
-            scalarVecMult(distance, {ResultStore[0], ResultStore[1], ResultStore[2]});
+            // Extract the light colour
+            for (i = 0; i < 3; i += 1)
+                vector[i] = Light[LightColour + i];
+            vecMult(textureColour, vector);
+            for (i = 0; i < 3; i += 1)
+                vector[i] = ResultStore[i];
+            scalarVecMult(distance, ResultStore);
         }
     }
     else
