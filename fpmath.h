ff29b0381efbd012d27bc2816ba7da796903005a
Andrew Hills <ahills60@gmail.com>
2014-07-29 16:15:54 +0100

    Adds division-less exponential function to fpmath
    - Function for exponential function added that doesn’t use division. It
    does lack some precision compared with original exponential method, but
    is approximately correct.

--- fpmath.h
+++ fpmath.h
@@ -59,12 +59,106 @@ float fp_cos(float a)
     a += (a < -FP_PI) * FP_2PI;
     // Use the sine function
     return fp_sin(a + FP_PI_2);
+}
+
+float exp(float z) 
+{
+  int t;
+  int x = bitset(z);
+  int y = 0x00010000;  /* 1.0 */
+  
+  t=x-0x58b91;   /* 5.5452 */ 
+  if (t>=0) 
+  {
+    x=t;
+	y<<=8;
+  }
+  t=x-0x2c5c8;   /* 2.7726 */
+  if (t>=0) 
+  {
+    x=t;
+	y<<=4;
+  }
+  t=x-0x162e4;  /* 1.3863 */
+  if (t>=0) 
+  {
+    x=t;
+	y<<=2;
+  }
+  t=x-0x0b172;  /* 0.6931 */
+  if (t>=0) 
+  {
+    x=t;
+	y<<=1;
+  }
+  t=x-0x067cd;  /* 0.4055 */
+  if (t>=0)
+  {
+    x=t;
+	y+=y>>1;
+  }
+  t=x-0x03920;  /* 0.2231 */
+  if (t>=0)
+  {
+    x=t;
+	y+=y>>2;
+  }
+  t=x-0x01e27;  /* 0.1178 */
+  if (t>=0)
+  {
+    x=t;
+	y+=y>>3;
+  }
+  t=x-0x00f85;  /* 0.0606 */
+  if (t>=0)
+  {
+    x=t;
+	y+=y>>4;
+  }
+  t=x-0x007e1;  /* 0.0308 */
+  if (t>=0) 
+  {
+    x=t;
+	y+=y>>5;
+  }
+  t=x-0x003f8;  /* 0.0155 */
+  if (t>=0) 
+  {
+    x=t;
+	y+=y>>6;
+  }
+  t=x-0x001fe;  /* 0.0078 */
+  if (t>=0) 
+  {
+    x=t;
+	y+=y>>7;
+  }
+  
+  if (x&0x100)
+    y+=y>>8;
+  if (x&0x080)
+    y+=y>>9;
+  if (x&0x040)
+    y+=y>>10;
+  if (x&0x020)
+    y+=y>>11;
+  if (x&0x010)
+    y+=y>>12;
+  if (x&0x008)
+    y+=y>>13;
+  if (x&0x004)
+    y+=y>>14;
+  if (x&0x002)
+    y+=y>>15;
+  if (x&0x001)
+    y+=y>>16;
+  return bitset(y);
+}
+
 float fp_log(float a)
 {
     if (a <= 0)
-    {
         return MIN_VAL;
-    }
     
     // Get the MSB position
     int im, j2, j1, j3, p = -16;
